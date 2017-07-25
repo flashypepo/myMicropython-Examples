f0b8e2f784024f8bc1dc2c9dd0159399c0a34f65
PePo <peter@pepo.nl>
2017-07-25 17:10:57 +0200

    added conway

---
+++ demos/neopixels/main.py
@@ -0,0 +1,8 @@
+# main - select which programma should startup
+import sys
+print(sys.implementation) # MP version
+#import blinky
+#import breathingLED
+#import knightrider
+#import pulseLED  #2017-0725
+import conway    #2017-25
