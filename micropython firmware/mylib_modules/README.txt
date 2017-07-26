fe5dfa9a88aa2c99412e9c210bc3fcc8c8bab98b
PePo <peter@pepo.nl>
2017-07-26 09:15:54 +0200

    updated frozen modules
    module to be frozen into the firmware, or separately.

---
+++ micropython firmware/mylib_modules/README.txt
@@ -0,0 +1,14 @@
+My MicroPython modules to compile with tool ‘mpy-cross’ as frozen modules. Preferable included into the firmware.
+
+2017-07** 
+add sdcard.py
+
+2017-0522
+Used in ESP32 building firmware. See VM: folder esp32/modules
+
+Files:
+ili9341.py              rgb.py
+bitmapfont.py           max7219.py              servo.py
+bme280.py               motor.py                ssd1306.py
+gfx.py                  neopixelchain.py        stepper.py
+ht16k33_matrix.py       pca9685.py              urtc.py
---
+++ micropython firmware/mylib_modules/ht16k33_matrix.py
@@ -0,0 +1,96 @@
+_HT16K33_BLINK_CMD = const(0x80)
+_HT16K33_BLINK_DISPLAYON = const(0x01)
+_HT16K33_CMD_BRIGHTNESS = const(0xE0)
+_HT16K33_OSCILATOR_ON = const(0x21)
+
+
+class HT16K33:
+    def __init__(self, i2c, address=0x70):
+        self.i2c = i2c
+        self.address = address
+        self._temp = bytearray(1)
+        self.buffer = bytearray(16)
+        self.fill(0)
+        self._write_cmd(_HT16K33_OSCILATOR_ON)
+        self.blink_rate(0)
+        self.brightness(15)
+
+    def _write_cmd(self, byte):
+        self._temp[0] = byte
+        self.i2c.writeto(self.address, self._temp)
+
+    def blink_rate(self, rate=None):
+        if rate is None:
+            return self._blink_rate
+        rate = rate & 0x03
+        self._blink_rate = rate
+        self._write_cmd(_HT16K33_BLINK_CMD |
+                        _HT16K33_BLINK_DISPLAYON | rate << 1)
+
+    def brightness(self, brightness):
+        if brightness is None:
+            return self._brightness
+        brightness = brightness & 0x0F
+        self._brightness = brightness
+        self._write_cmd(_HT16K33_CMD_BRIGHTNESS | brightness)
+
+    def show(self):
+        self.i2c.writeto_mem(self.address, 0x00, self.buffer)
+
+    def fill(self, color):
+        fill = 0xff if color else 0x00
+        for i in range(16):
+            self.buffer[i] = fill
+
+    def _pixel(self, x, y, color=None):
+        mask = 1 << x
+        if color is None:
+            return bool((self.buffer[y] | self.buffer[y + 1] << 8) & mask)
+        if color:
+            self.buffer[y * 2] |= mask & 0xff
+            self.buffer[y * 2 + 1] |= mask >> 8
+        else:
+            self.buffer[y * 2] &= ~(mask & 0xff)
+            self.buffer[y * 2 + 1] &= ~(mask >> 8)
+
+
+class Matrix16x8(HT16K33):
+    def pixel(self, x, y, color=None):
+        if not 0 <= x <= 15:
+            return
+        if not 0 <= y <= 7:
+            return
+        if x >= 8:
+            x -= 8
+            y += 8
+        return super()._pixel(y, x, color)
+
+
+class Matrix8x8(HT16K33):
+    def pixel(self, x, y, color=None):
+        if not 0 <= x <= 7:
+            return
+        if not 0 <= y <= 7:
+            return
+        x = (x - 1) % 8
+        return super()._pixel(x, y, color)
+
+
+class Matrix8x8x2(HT16K33):
+    def pixel(self, x, y, color=None):
+        if not 0 <= x <= 7:
+            return
+        if not 0 <= y <= 7:
+            return
+        if color is not None:
+            super()._pixel(y, x, (color & 0x01))
+            super()._pixel(y + 8, x, (color >> 1) & 0x01)
+        else:
+            return super()._pixel(y, x) | super()._pixel(y + 8, x) << 1
+
+    def fill(self, color):
+        fill1 = 0xff if color & 0x01 else 0x00
+        fill2 = 0xff if color & 0x02 else 0x00
+        for i in range(8):
+            self.buffer[i * 2] = fill1
+            self.buffer[i * 2 + 1] = fill2
