911f9acd7d220e99783e89d3f30c0e4c1c0deaa2
PePo <peter@pepo.nl>
2017-08-03 17:04:30 +0200

    TMP36 experiment
    Demonstration of using micro python to display temperature (Cesius and
    Fahrenheit) of a TMP36 temperature sensor  OLED SSD1306 display. Data
    sheet and changes in main.py. Part of SmartDevices.

---
+++ temperature/main_temp.py
@@ -0,0 +1,9 @@
+# main.py for testing OLED-displays
+# 2017-0728 PePo new
+
+''' import oledssd1306 as test
+print('re-set device, run test.demo()')
+#'''
+#''' tmp36 experiment
+#import temperature.tmp36 #zonder OLED
+import temperature.tmp36_oled
--- temperature/tmp36_oled.py
+++ temperature/tmp36_oled.py
@@ -1,4 +1,5 @@
 ''' TMP36 temperature  sensor
+Let op: verwissel Vs en GND niet van de TMP36!
   pin TMP36   NodeMCU  Huzzah ESP8266
   data-out    A0       ADC
   Vs          3.3V       3.3V
@@ -6,12 +7,14 @@
 #'''
 
 import machine, time
+import ssd1306
+
 # using from machine import ADC is bad practice
 
 # calculate temperature (celsius) from voltage value (mV)
 def temp(value):
-	#NOT_VALID? 
-    return (value - 500) / 10 #Huzzah
+    _OFFSET = const(20) #calibration factor, should be 0
+    return (value - 500 - _OFFSET) / 10 #Huzzah
 	#NodeMCU: return (value) / 10
 
 # Celsius to Fahrenheit
@@ -19,39 +22,50 @@ def fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
 _ADC_PIN = 0
-_WARNING_LED_PIN = 4
-_RELAY_PIN = 5
+_WARNING_LED_PIN = 14
+
+# create i2c for display
+i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=100000)
+print('i2c.scan: ', i2c.scan())   #[60]
+# OLED screen dimensions
+__WIDTH = const(128) 
+__HEIGHT = const(32)
+oled = ssd1306.SSD1306_I2C(__WIDTH, __HEIGHT, i2c)
 
 # program
 # read voltage (mV)
 adc = machine.ADC(_ADC_PIN)
 #TEST: print('ADC reading:', adc.read())
 alert = machine.Pin(_WARNING_LED_PIN, machine.Pin.OUT)
-relais = machine.Pin(_RELAY_PIN, machine.Pin.OUT)
 
 # alert ON and OFF
 def alertOn():
     alert.on()
-    relais.on()
 
 def alertOff():
     alert.off()
-    relais.off()
 
 # run readng T, Ctrl-C to abort
+_TRESHOLD = const(30)
 def run(dt=2.0):
+    print('TMP36 demo on OLED')
     try:
         while True:
+            oled.fill(0)  # clear screen
             reading = adc.read()
             celsius_temp = temp(reading)
             fahrenheit_temp = fahrenheit(celsius_temp)
             print('TMP36 reading {}\tDegrees Celsius {}\tDegrees Fahrenheit {}'.format(reading, celsius_temp, fahrenheit_temp))
-            if celsius_temp > 26:
+            oled.text('TMP36 {0} '.format(reading),0,0)
+            oled.text('Celsius {0:0.1f}'.format(celsius_temp),0,10)
+            oled.text('Fahrenheit {0:0.1f}'.format(fahrenheit_temp),0,20)
+            if celsius_temp > _TRESHOLD:
                 alertOn()
             else:
                 alertOff()
+            oled.show()
             time.sleep(dt) #wait > s, see datasheet
     except:
-        print('done')
+        print('Exception! Done')
 
-run()
+run(5.0)
