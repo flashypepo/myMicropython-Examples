59336259fb47a2a51d0fdbcc6068510b0a20a80c
PePo <peter@pepo.nl>
2017-08-06 00:29:08 +0200

    changed pin of LED - not tested

--- display-stuff/leds/blinky.py
+++ display-stuff/leds/blinky.py
@@ -1,4 +1,4 @@
-# blinky.py - blink LED aan/uit 
+# blinky.py - blink LED aan/uit
 # _LED_PIN = 5: WeMOS-LoLin32, zie achterzijde van de development board
 # _LED_PIN = 4: NodeMCU
 # _LED_PIN = 2: WeMOS D1 R2, 2017_0728
@@ -7,7 +7,7 @@ from machine import Pin
 import time
 
 #_LED_PIN = 15  # NodeMCU
-_LED_PIN = 2  # WeMOS D1 R2 / ESP8266
+_LED_PIN = 0#2  # WeMOS D1 R2 / ESP8266
 
 led = Pin(_LED_PIN, Pin.OUT)
 
@@ -22,7 +22,7 @@ for i in range(10):
    led.value(1)
    time.sleep(0.25)
 # end led off
-off() 
+off()
 '''
 def blink(dt=0.1):
     led.on()
@@ -34,7 +34,7 @@ def blink(dt=0.1):
 for i in range(10):
     #blink()
     blink(0.2)
-    
+
 led.on() #WeMOS D1 R2, pulldown!
 #led.off() #otherwise
 print('done!')
